f6d9cab4a791fd31936e90230bdde10a4c8b8f39
Simon Prykhodko <semion.ababo@gmail.com>
2020-12-13 19:37:44 +0100

    Implement buddy allocator construction

--- src/buddy.rs
+++ src/buddy.rs
@@ -1,17 +1,8 @@
+use core::intrinsics::{copy, write_bytes};
 use core::mem::size_of;
 use core::ptr::null;
 
 use super::common::*;
-
-#[inline]
-fn align_addr_lower(addr: usize, order: u8) -> usize {
-    addr >> order << order
-}
-
-#[inline]
-fn align_addr_higher(addr: usize, order: u8) -> usize {
-    align_addr_lower(addr + (1 << order) - 1, order)
-}
 
 struct Link {
     next: *mut Link,
@@ -21,8 +12,12 @@ struct Link {
 struct BuddyAlloc {
     min_order: u8,
     num_orders: u8,
+    map_from_addr: usize,
+    map_factor: usize,
     data: *mut *mut Link,
 }
+
+const USIZE_BITS: usize = usize::BITS as usize;
 
 impl BuddyAlloc {
     pub unsafe fn new(
@@ -46,45 +41,112 @@ impl BuddyAlloc {
             order -= 1;
         }
 
-        let num_orders = order - min_order + 1;
-        let heads_size = (num_orders as usize) * size_of::<*mut Link>();
-        let map_len = (1 << (num_orders + 1)) - 2;
-        let map_size = (map_len - 1) / 8 + 1;
-        if to_addr - from_addr < size_of::<Link>() + heads_size + map_size {
-            return Err(Error::NotEnoughSpaceForBookkeeping);
-        }
+        let map_from_addr = align_addr_lower(addr, order);
+        let map_to_addr = align_addr_higher(addr + size, order);
 
         let mut alloc = BuddyAlloc {
             min_order: min_order,
-            num_orders: num_orders,
+            num_orders: order - min_order + 1,
+            map_from_addr: map_from_addr,
+            map_factor: (map_to_addr - map_from_addr) >> order,
             data: (from_addr + size_of::<Link>()) as *mut *mut Link,
         };
 
-        alloc.set_map_bit(order, 0, true);
-        alloc.set_map_bit(order, 1, false);
-        *alloc.data = from_addr as *mut Link;
-        **alloc.data = Link {
-            next: null::<Link>() as *mut Link,
-            prev: null::<Link>() as *mut Link,
-        };
+        let heads_size = (alloc.num_orders as usize) * size_of::<*mut Link>();
+        let map_len = alloc.map_order_offset(min_order - 1);
+        let map_size = ((map_len - 1) / USIZE_BITS + 1) * size_of::<usize>();
+        let data_size = heads_size + map_size;
+        if to_addr - from_addr < size_of::<Link>() + data_size {
+            return Err(Error::NotEnoughSpaceForBookkeeping);
+        }
 
-        loop {
-            order -= 1;
+        write_bytes(alloc.data.offset(alloc.num_orders as isize), 0, map_size);
+        *alloc.data = null::<*mut Link>() as *mut Link;
+        alloc.add_free_block(order, from_addr);
+
+        for order in order - 1..=min_order {
+            *alloc.data.offset((order - alloc.min_order) as isize) =
+                null::<*mut Link>() as *mut Link;
+
             let ptr = align_addr_higher(addr, order);
-            if ptr != from_addr {}
+            if ptr != from_addr {
+                alloc.add_free_block(order, ptr);
+            }
+            from_addr = ptr;
 
             let ptr = align_addr_lower(addr + size, order);
-            if ptr != to_addr {}
+            if ptr != to_addr {
+                alloc.add_free_block(order, ptr);
+            }
+            to_addr = ptr;
         }
+
+        let data = alloc.alloc(size_order(data_size))? as *mut *mut Link;
+        copy(data, alloc.data, data_size / size_of::<*mut Link>());
+        alloc.data = data;
 
         Ok(alloc)
     }
 
     #[inline]
-    fn map_bit(&self, order: u8, index: usize) -> bool {
-        false
+    fn map_order_offset(&self, order: u8) -> usize {
+        let order_index = order - self.min_order;
+        if order_index > 0 {
+            1 + self.map_factor * ((1 << (order_index - 1)) - 1)
+        } else {
+            0 // The first bit is always for the highest order pair.
+        }
     }
 
     #[inline]
-    fn set_map_bit(&mut self, order: u8, index: usize, value: bool) {}
+    fn map_index(&self, order: u8, ptr: usize) -> usize {
+        self.map_order_offset(order)
+            + ((ptr - self.map_from_addr) >> (order + 1))
+    }
+
+    #[inline]
+    unsafe fn map_bit(&self, order: u8, ptr: usize) -> bool {
+        let index = self.map_index(order, ptr);
+        let base = self.data.offset(self.num_orders as isize) as *const usize;
+        let block = *base.offset((index / USIZE_BITS) as isize);
+        let mask = 1 << (index % USIZE_BITS);
+        block & mask != 0
+    }
+
+    #[inline]
+    unsafe fn set_map_bit(&mut self, order: u8, ptr: usize, value: bool) {
+        let index = self.map_index(order, ptr);
+        let base = self.data.offset(self.num_orders as isize) as *mut usize;
+        let block_ptr = base.offset((index / USIZE_BITS) as isize);
+        let mask = 1 << (index % USIZE_BITS);
+        if value {
+            *block_ptr |= mask;
+        } else {
+            *block_ptr &= !mask;
+        }
+    }
+
+    #[inline]
+    unsafe fn add_free_block(&mut self, order: u8, ptr: usize) {
+        let head = self.data.offset((order - self.min_order) as isize);
+        let link = ptr as *mut Link;
+        *link = Link {
+            next: *head,
+            prev: null::<Link>() as *mut Link,
+        };
+        if *head != null::<Link>() as *mut Link {
+            (**head).prev = link;
+        }
+        *head = link;
+
+        self.set_map_bit(order, ptr, true);
+    }
+
+    pub fn alloc(&mut self, order: u8) -> Result<usize> {
+        Ok(0)
+    }
+
+    pub unsafe fn free(&mut self, order: u8, addr: usize) -> Result<()> {
+        Ok(())
+    }
 }
--- src/common.rs
+++ src/common.rs
@@ -4,3 +4,23 @@ pub enum Error {
 }
 
 pub type Result<T> = core::result::Result<T, Error>;
+
+#[inline]
+pub fn align_addr_lower(addr: usize, order: u8) -> usize {
+    addr >> order << order
+}
+
+#[inline]
+pub fn align_addr_higher(addr: usize, order: u8) -> usize {
+    align_addr_lower(addr + (1 << order) - 1, order)
+}
+
+#[inline]
+pub fn size_order(size: usize) -> u8 {
+    let order = (usize::BITS - size.leading_zeros() - 1) as u8;
+    if size == (1 << order) {
+        order
+    } else {
+        order + 1
+    }
+}
